47fcc82d12a20a8813eeaab09b17b1a19dc2474d
Dennis Schubert <software@dsx.cc>
2011-07-09 01:46:10 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schubio::Application.config.secret_token = '8cf3f09c1ae6984ae9dadd6e94334a5713483aaf6334c47c1d4a31df6b7b13910b4f8ec79c57c1701a4733f3f6d19f46b609b108cc2473e454ba51aa5d46d153'
