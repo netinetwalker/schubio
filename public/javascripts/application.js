a8f420694e5a1469e1aa61adf26084a0e0c1d9db
Dennis Schubert <software@dsx.cc>
2011-07-11 12:19:17 +0200

    Added facebox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,38 +1,43 @@
 $(document).ready(function(){
-    tipsyInit();
-    magicBarInit();
+  tipsyInit();
+  fancyboxInit();
+  magicBarInit();
 });
 
 function tipsyInit() {
-    $("#footboxRight div a").tipsy({gravity: 's'});
-    $("#headerSocial a").tipsy({gravity: 'n'});
+  $("#footboxRight div a").tipsy({gravity: 's'});
+  $("#headerSocial a").tipsy({gravity: 'n'});
+}
+
+function fancyboxInit() {
+  $('a.faceboxLink').facebox()
 }
 
 function magicBarInit() {
-    magicBarIconClickInit();
-    magicBarChangeModeInit();
+  magicBarIconClickInit();
+  magicBarChangeModeInit();
 }
 
-    function magicBarIconClickInit() {
-        $("#mbTermIcon").click(function() {
-            $("#magicbar").submit();
-        });
+  function magicBarIconClickInit() {
+    $("#mbTermIcon").click(function() {
+      $("#magicbar").submit();
+    });
+  }
+  
+  function magicBarChangeModeInit() {
+    $("#mbTerm").keyup(function() {
+      if($("#mbTerm").val().substring(1,0) == "!") {
+        magicBarChangeModeChange(true);
+      } else {
+        magicBarChangeModeChange(false);
+      }
+    });
+  }
+  
+  function magicBarChangeModeChange(executeMode) {
+    if (executeMode) {
+      $("#mbTermIcon").addClass("execute");
+    } else {
+      $("#mbTermIcon").removeClass("execute");
     }
-    
-    function magicBarChangeModeInit() {
-        $("#mbTerm").keyup(function() {
-            if($("#mbTerm").val().substring(1,0) == "!") {
-                magicBarChangeModeChange(true);
-            } else {
-                magicBarChangeModeChange(false);
-            }
-        });
-    }
-    
-    function magicBarChangeModeChange(executeMode) {
-        if (executeMode) {
-            $("#mbTermIcon").addClass("execute");
-        } else {
-            $("#mbTermIcon").removeClass("execute");
-        }
-    }
+  }
