b7cb401b34e8665b22e62982ea0383b5b52a6f0a
Dennis Schubert <software@dsx.cc>
2011-08-21 14:29:08 +0200

    Ehh...

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,7 +1,6 @@
 class Blogpost < ActiveRecord::Base
   has_many :blogpost_comments
   acts_as_taggable
-  validates_associated :blogpost_comments
 
   def to_param
     "#{id}-#{title.parameterize}"
