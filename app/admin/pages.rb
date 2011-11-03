3b9689608303ad28614776f598abaae8488846e3
Dennis Schubert <software@dsx.cc>
2011-11-03 21:16:58 +0100

    Update Gems and build the admin-form manually to prevent html5's validation.

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -6,4 +6,13 @@ ActiveAdmin.register Page do
 
     default_actions
   end
+
+  form do |f|
+    f.inputs do
+      f.input :title, :as => :string
+      f.input :url, :as => :string
+      f.input :content, :as => :text
+    end
+    f.buttons
+  end
 end
