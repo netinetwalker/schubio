15929f40738e5d525894a1ce80314aa416fc0274
Dennis Schubert <software@dsx.cc>
2012-12-06 01:10:32 +0100

    Order projects!

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,11 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all()
+    @projects = Project.order(:title).all()
     @introduction = Page.find_by_url("projects_introduction")
   end
 
   def show
-    @projects = Project.all()
+    @projects = Project.order(:title).all()
     @project = Project.find(params[:id])
   end
 end
