baa74809d49832af11247b2f8e80e7665402de0a
Dennis Schubert <software@dsx.cc>
2013-01-14 23:48:46 +0100

    A bit nicer devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Schubio::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :user
 
-  resources :projects
-
   match "blog/tag/:id(/page/:page)" => "blog#show_tag", :as => :blogtag
   match "blog/page/:page" => "blog#index"
   match "blog/feed" => "blog#feed", :as => :blog_feed
   match "blog/comments/feed" => "blogpost_comments#feed", :as => :blog_comments_feed
+
   resources :blogposts, :controller => "blog", :path => "blog" do
     resources :blogpost_comments, :path => "comments"
   end
 
   # routes for the pages. takes pretty much everything except paths with / inside
+  resources :projects
   resources :pages
   match ":id" => "pages#show", :as => :page_clean
 
